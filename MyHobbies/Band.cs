92f786e32273ccca5aed5840bfc7f55dc3b777d6
SenesaelRobbe <robbe.senesael@student.howest.be>
2018-02-19 12:28:09 +0100

    Made Band & BandLid classes & MyHobbies Namespace

---
+++ MyHobbies/Band.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHobbies
+{
+    class Band
+    {
+        public int Jaar { get; set; }
+        public string Naam{get; set; }
+
+        List<BandLid> band = new List<BandLid>();
+    }
+}
---
+++ MyHobbies/BandLid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHobbies
+{
+    class BandLid
+    {
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+        public string Geslacht { get; set; }
+
+        public BandLid(string naam, int leeftijd, string geslacht)
+        {
+            naam = Naam;
+            leeftijd = Leeftijd;
+            geslacht = Geslacht;
+        }
+    }
+}
