9ecf50b5147bba6e8e85e20d8a132ee8f24101ad
SenesaelRobbe <robbe.senesael@student.howest.be>
2018-02-19 12:21:09 +0100

    Add project files.

---
+++ Les02_Oefeningenreeks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Les02_Oefeningenreeks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
